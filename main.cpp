ffe48cf60ab033897c086c3c5fc3bbbd558fdc9a
neoname gaming <38029392+neoname@users.noreply.github.com>
2023-01-18 07:50:03 +0000

    i tried my best

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 
+void game(){
+	std::cout<<"merge";
+}
+
 int main(){
 
-	int valoare = 500;
-	int pariu;
-	
-	
+	bool joci;
 	std::cout<<"Bine ai venit la Pacaneaua lu' neo"<<std::endl;
-	std::cout<<"Ai "<<valoare<<" euro."
-	std::cout<<"Baga cat vrei sa pierzi"<std::endl;
-	
+	std::cout<<"Joci? (Y/N)";
+	std::cin>>joci;
+	if(joci){
+		std::cout<<"Mult noroc bai pula"<<std::endl;
+		game();
+	}
 
 	return 0;
 }
+
+
